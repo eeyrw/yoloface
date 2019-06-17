21fbbea55106cffaa70760e8b464d0a488bda225
yuan <yrw@sdec.com.cn>
2019-06-17 16:25:32 +0800

    M

--- validation.py
+++ validation.py
@@ -95,12 +95,13 @@ if __name__ == "__main__":
 
 
     imageList=list(wider.next())
-    for i,item in enumerate(imageList[0:-1]):
+    for i,item in enumerate(imageList[700:-1]):
         image = Image.open(item.image_name)
         res_image, _ = yolo.detect_image(image)
+        print(_)
         draw = ImageDraw.Draw(res_image)
         for bbox in item.bboxes:
-            #print(bbox)
+            print(bbox)
             draw.rectangle([bbox[0], bbox[1], bbox[2],  bbox[3]],outline=(255, 0, 0))
         del draw
         #image.show()
